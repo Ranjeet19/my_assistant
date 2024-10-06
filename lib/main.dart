dc0d176a44ec23769d7f23103e31d01a09dbc6f0
Ranjeet Shrestha <shrestharanjit2014@gmail.com>
2024-10-06 10:10:51 +0100

    update

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         scaffoldBackgroundColor: mobileBackgroundColor, // Set global background color here
       ),
 
-      home:   HomePage(),
+      home:const HomePage(),
     );
   }
 }
